0430d3e25744eb51da85c089fada2493d4da190d
SGoyalascent <shubham@ascentspark.com>
2021-12-14 16:11:06 +0530

    Agent

--- Primary_RAgent.c
+++ Primary_RAgent.c
@@ -33,12 +33,12 @@ char execpath[256];
 
 struct timestamp {
 
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minutes;
-    int second;
+    unsigned char year;
+    unsigned char month[1];
+    unsigned char day[1];
+    unsigned char hour[1];
+    unsigned char minutes[1];
+    unsigned char second[1];
 };
 
 struct timestamp tm;
@@ -57,28 +57,28 @@ void Call_ReportChanges_Mirror() {
         buffer[REQUEST_HEADER +i ] = i+1;
     }
 
-    ts.val = tm.year - 2000;
-    printf("%d %d\n", ts.val, ts.byte[0]);
-    buffer[REQUEST_HEADER + CH_BYTES_CNT] = ts.byte[0];
+    //ts.val = tm.year - 2000;
+    //printf("%d %d\n", ts.val, ts.byte[0]);
+    buffer[REQUEST_HEADER + CH_BYTES_CNT] = tm.year - 2000;
 
-    ts.val = tm.month;
-    printf("%d %d\n", ts.val, ts.byte[0]);
-    buffer[REQUEST_HEADER + CH_BYTES_CNT + 1] = ts.byte[0];
+    //ts.val = tm.month;
+    //printf("%d %d\n", ts.val, ts.byte[0]);
+    buffer[REQUEST_HEADER + CH_BYTES_CNT + 1] = tm.month[0];
 
     ts.val = tm.day;
-    printf("%d %d\n", ts.val, ts.byte[0]);
+    //printf("%d %d\n", ts.val, ts.byte[0]);
     buffer[REQUEST_HEADER + CH_BYTES_CNT + 2] = ts.byte[0];
 
     ts.val = tm.hour;
-    printf("%d %d\n", ts.val, ts.byte[0]);
+    //printf("%d %d\n", ts.val, ts.byte[0]);
     buffer[REQUEST_HEADER + CH_BYTES_CNT + 3] = ts.byte[0];
 
     ts.val = tm.minutes;
-    printf("%d %d\n", ts.val, ts.byte[0]);
+    //printf("%d %d\n", ts.val, ts.byte[0]);
     buffer[REQUEST_HEADER + CH_BYTES_CNT + 4] = ts.byte[0];
 
     ts.val = tm.second;
-    printf("%d %d\n", ts.val, ts.byte[0]);
+    //printf("%d %d\n", ts.val, ts.byte[0]);
     buffer[REQUEST_HEADER + CH_BYTES_CNT + 5] = ts.byte[0];
 
     printf("send_buffer:- ");
@@ -122,42 +122,10 @@ void getLastModifiedTime() {
     tm.minutes = dt->tm_min;
     tm.second = dt->tm_sec;
     printf("local time: ");
-    printf("Last Modified Time1:- %d-%d-%d  %d:%d:%d\n",dt->tm_mday,dt->tm_mon,dt->tm_year, dt->tm_hour,dt->tm_min, dt->tm_sec);
-    printf("Last Modified Time2:- %d-%d-%d  %d:%d:%d\n",dt->tm_mday,dt->tm_mon,dt->tm_year+1900, dt->tm_hour,dt->tm_min, dt->tm_sec);
+    //printf("Last Modified Time2:- %d-%d-%d  %d:%d:%d\n",dt->tm_mday,dt->tm_mon,dt->tm_year+1900, dt->tm_hour,dt->tm_min, dt->tm_sec);
     printf("Last Modified Time3:- %d-%d-%d  %d:%d:%d\n",tm.day, tm.month,tm.year, tm.hour, tm.minutes, tm.second);
 
     Call_ReportChanges_Mirror();
-
-    dt = gmtime(&attrib.st_mtime);
-
-    tm.year = dt->tm_year + 1900;
-    tm.month = dt->tm_mon;
-    tm.day = dt->tm_mday;
-    tm.hour = dt->tm_hour;
-    tm.minutes = dt->tm_min;
-    tm.second = dt->tm_sec;
-    printf("gmt: ");
-    printf("Last Modified Time1:- %d-%d-%d  %d:%d:%d\n",dt->tm_mday,dt->tm_mon,dt->tm_year, dt->tm_hour,dt->tm_min, dt->tm_sec);
-    printf("Last Modified Time2:- %d-%d-%d  %d:%d:%d\n",dt->tm_mday,dt->tm_mon,dt->tm_year+1900, dt->tm_hour,dt->tm_min, dt->tm_sec);
-    printf("Last Modified Time3:- %d-%d-%d  %d:%d:%d\n",tm.day, tm.month,tm.year, tm.hour, tm.minutes, tm.second);
-
-    Call_ReportChanges_Mirror();
-
-    dt = localtime(&attrib.st_atime);
-
-    tm.year = dt->tm_year + 1900;
-    tm.month = dt->tm_mon;
-    tm.day = dt->tm_mday;
-    tm.hour = dt->tm_hour;
-    tm.minutes = dt->tm_min;
-    tm.second = dt->tm_sec;
-    printf("localtime: ");
-    printf("Last Access Time1:- %d-%d-%d  %d:%d:%d\n",dt->tm_mday,dt->tm_mon,dt->tm_year, dt->tm_hour,dt->tm_min, dt->tm_sec);
-    printf("Last Access Time2:- %d-%d-%d  %d:%d:%d\n",dt->tm_mday,dt->tm_mon,dt->tm_year+1900, dt->tm_hour,dt->tm_min, dt->tm_sec);
-    printf("Last Access Time3:- %d-%d-%d  %d:%d:%d\n",tm.day, tm.month,tm.year, tm.hour, tm.minutes, tm.second);
-
-    Call_ReportChanges_Mirror();
-
 
 }
 
