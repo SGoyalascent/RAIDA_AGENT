2204df64e6aa7fc73003804896da70ce06376a4f
SGoyalascent <shubham@ascentspark.com>
2021-12-24 21:08:05 +0530

    RAIDA_Agent

--- Primary_RAgent_Call_Services.c
+++ Primary_RAgent_Call_Services.c
@@ -2,20 +2,56 @@
 
 struct sockaddr_in servaddr, cliaddr;
 int sockfd = 0;
-unsigned char udp_buffer[UDP_BUFF_SIZE],response[RESPONSE_HEADER_MAX];
-unsigned char udp_response[MAXLINE], send_req_buffer[MAXLINE];
-unsigned char request_header[REQ_HEAD_MIN_LEN];
+unsigned char udp_buffer[UDP_BUFF_SIZE],send_req_buffer[MAXLINE], request_header[REQ_HEAD_MIN_LEN];
+unsigned char recv_response[RESPONSE_HEADER_MAX];
 int index_req = 0;
 time_t t1;
+
+
+//-----------------------------------------------------------
+//Initialize UDP Socket
+//-----------------------------------------------------------
+int init_udp_socket() {
+	
+	printf("init_udp_socket\n");
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	memset(&servaddr, 0, sizeof(servaddr));
+	//memset(&cliaddr, 0, sizeof(cliaddr));
+	
+	servaddr.sin_family = AF_INET; 
+	//servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_addr.s_addr = inet_addr(Mirror_agent_config.Ip_address); //Mirror ip address to send request
+	servaddr.sin_port = htons(Primary_agent_config.port_number);    //Primary port no.
+
+/*
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,sizeof(servaddr)) < 0 ){
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	*/
+}
+
+//-----------------------------------------------------------
+//Set time out for UDP frames
+//-----------------------------------------------------------
+void set_time_out(unsigned char secs){     
+	FD_ZERO(&select_fds);            
+	FD_SET(sockfd, &select_fds);           	                                  
+	timeout.tv_sec = secs; 
+	timeout.tv_usec = 0;
+}
 
 //-------------------------------------------------------
 //Request Header to Call Mirror Services
 //---------------------------------------------------------
-void prepare_send_req_header() {
+void prepare_send_req_header(unsigned char command_code) {
     
     request_header[REQ_CL] = 0;
     request_header[REQ_SP] = 0;
-    request_header[REQ_RI] = raida_id;    // raida_no.
+    request_header[REQ_RI] = server_config_obj.raida_id;    // raida_no.
     request_header[REQ_SH] = 0;
     request_header[REQ_CM] = 0;
     request_header[REQ_CM+1] = command_code;   //command code
@@ -25,7 +61,7 @@ void prepare_send_req_header() {
     request_header[REQ_RE] = 0;
     request_header[REQ_RE+1] = 0;
     request_header[REQ_RE+2] = 0;
-    request_header[REQ_EC] = 22;                 //
+    request_header[REQ_EC] = 22;                 //??Test again separetly??//
     request_header[REQ_EC+1] = 22;    
     request_header[REQ_FC] = 0;
     request_header[REQ_FC+1] = 1;    // udp packets sent
@@ -43,7 +79,6 @@ void prepare_send_req_header() {
     for(int i=0; i < CH_BYTES_CNT; i++) {
         send_req_buffer[index_req + i] = i+1;
     }
-
     index_req += CH_BYTES_CNT;
 
 }
@@ -51,52 +86,18 @@ void prepare_send_req_header() {
 //--------------------------------------------------------
 //Send Request to Mirror Raida
 //---------------------------------------------------------
-void Send_Request_Mirror() {
-	sendto(sockfd, (const char *)buffer, len,
-	        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
-	            sizeof(servaddr));
+void Send_Request(unsigned char command_code, unsigned int size) {
+	char * myfifo = "/tmp/myfifo";
+	prepare_send_req_header(command_code);
+	sendto(sockfd, (const char *)send_req_buffer, size,
+	        MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
 
-
-}
-
-//-----------------------------------------------------------
-//Set time out for UDP frames
-//-----------------------------------------------------------
-void set_time_out(unsigned char secs){     
-	FD_ZERO(&select_fds);            
-	FD_SET(sockfd, &select_fds);           	                                  
-	timeout.tv_sec = secs; 
-	timeout.tv_usec = 0;
-}
-
-//-----------------------------------------------------------
-//Initialize UDP Socket and bind to the port
-//-----------------------------------------------------------
-int init_udp_socket() {
-	
-	printf("init_udp_socket\n");
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-		perror("socket creation failed");
-		exit(EXIT_FAILURE);
-	}
-	memset(&servaddr, 0, sizeof(servaddr));
-	//memset(&cliaddr, 0, sizeof(cliaddr));
-	
-	servaddr.sin_family = AF_INET; 
-	//servaddr.sin_addr.s_addr = INADDR_ANY;
-	servaddr.sin_addr.s_addr = inet_addr(Mirror_agent_config.Ip_address); //Mirror ip address to send request
-	servaddr.sin_port = htons(Primary_agent_config.port_number);    //Primary port no.
-
-	if ( bind(sockfd, (const struct sockaddr *)&servaddr,sizeof(servaddr)) < 0 ){
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
 }
 
 //-----------------------------------------------------------
 //Receive Response from Mirror Raida
 //-----------------------------------------------------------
-void Receive_response_Mirror_() {
+void Receive_response() {
     unsigned char *buffer,state = STATE_WAIT_START,status_code;
 	uint16_t frames_expected=0,curr_frame_no=0,n=0,index=0;
 	//uint32_t client_s_addr=0; 	
@@ -118,14 +119,14 @@ void Receive_response_Mirror_() {
 				printf("---------------------RESPONSE HEADER RECEIVED ----------------------------\n");
 				status_code = validate_response_header(buffer,n);
 				if(status_code != NO_ERR_CODE){
-					send_err_resp_header_toMirror(status_code);			
+					printf("Error: Response Header not validated. Error_no: %s\n", status_code);			
 					state = STATE_WAIT_START;
 				}
 				else {
 					frames_expected = buffer[RES_RE+1];
 					frames_expected|=(((uint16_t)buffer[RES_RE])<<8); 
 					printf("frames expected: %d\n", frames_expected);
-					memcpy(udp_buffer,buffer,n);
+					memcpy(recv_response,buffer,n);
 					index = n;
 					if(frames_expected == 1){
 						state = STATE_END_RECVD;
@@ -137,13 +138,13 @@ void Receive_response_Mirror_() {
 			case STATE_WAIT_END:
 				set_time_out(FRAME_TIME_OUT_SECS);
 				if (select(32, &select_fds, NULL, NULL, &timeout) == 0 ){
-					send_err_resp_header_toMirror(FRAME_TIME_OUT);
+					//send_err_resp_header_toMirror(FRAME_TIME_OUT);
 					state = STATE_WAIT_START;
 					printf("Time out error \n");
 				}
 				else {
 					n = recvfrom(sockfd, (unsigned char *)buffer, server_config_obj.bytes_per_frame,MSG_WAITALL, ( struct sockaddr *) &servaddr,&len);
-						memcpy(&udp_buffer[index],buffer,n);
+						memcpy(&recv_response[index],buffer,n);
 						index += n;
 						curr_frame_no++;
 						printf("--------RECVD  FRAME NO ------ %d\n", curr_frame_no);
@@ -154,23 +155,20 @@ void Receive_response_Mirror_() {
 					
 			break;			
 			case STATE_END_RECVD:
-				if(udp_buffer[index-1]!=REQ_END|| udp_buffer[index-2]!=REQ_END){
-					send_err_resp_header_toMirror(INVALID_END_OF_REQ);
-					printf("--Invalid end of packet  \n");
+				if(recv_response[index-1] != RESP_END|| recv_response[index-2] != RESP_END){
+					printf("--Error: Invalid end of Response packet  \n");
 				}
 				else {
 					printf("---------------------END RECVD----------------------------------------------\n");
 					printf("---------------------PROCESSING RESPONSE-----------------------------\n");
-					process_request(index);
 				}
-				state = STATE_WAIT_START;
 			break;
 		}
 	}
 }
 
 //-----------------------------------------------------------
-//  Validate request header
+//  Validate Response header
 //-----------------------------------------------------------
 unsigned char validate_response_header(unsigned char * buff,int packet_size){
 	uint16_t frames_expected,resp_header_exp_len= RESP_HEADER_MIN_LEN;
@@ -190,114 +188,30 @@ unsigned char validate_response_header(unsigned char * buff,int packet_size){
 		printf("Invalid Raida id \n");
 		return WRONG_RAIDA;
 	}
-	if(buff[RES_EC] != && buff[RES_EC+1] != ) {
-
-
+	if((buff[RES_EC] != send_req_buffer[REQ_EC]) || (buff[RES_EC+1] != send_req_buffer[REQ_EC+1])) {
+		printf("Error: EC bytes not match\n");
 	}
 	return NO_ERR_CODE;
 }
 
 //------------------------------------------------------------------------------------------
-//  Validate coins and request body and return number of coins 
+//  Validate Response body 
 //-----------------------------------------------------------------------------------------
-unsigned char validate_request_body_general(unsigned int packet_len,unsigned int req_body,int *req_header_min){
-	*req_header_min = REQ_HEAD_MIN_LEN;
-	if(packet_len != (*req_header_min) + req_body){
-		send_err_resp_header(INVALID_PACKET_LEN);
+unsigned char validate_resp_body_general(unsigned int packet_len,unsigned int resp_body,int *resp_header_min){
+	*resp_header_min = RESP_HEADER_MIN_LEN;
+	if(packet_len != (*resp_header_min) + resp_body){
+		printf("Error: Response body not validated. Error: Invalid End of packet\n ");
 		return 0;
 	}
 	return 1;
 }
 
-//-----------------------------------------------------------
-// Processes the UDP packet 
-//-----------------------------------------------------------
-void process_request(unsigned int packet_len){
-	uint16_t cmd_no=0, coin_id;
-	time_stamp_before = get_time_cs();
-	memset(response,0,RESPONSE_HEADER_MAX-1);
-	cmd_no = udp_buffer[REQ_CM+1];
-	cmd_no |= (((uint16_t)udp_buffer[REQ_CM])<<8);
-	coin_id = udp_buffer[REQ_CI+1];
-	coin_id |= (((uint16_t)udp_buffer[REQ_CI])<<8);
-	switch(cmd_no){
-	
-		case CMD_COIN_CONVERTER : 			execute_coin_converter(packet_len);break;
-		//case CMD_ECHO:						execute_echo(packet_len);break;
-		default:							send_err_resp_header(INVALID_CMD);	
-	}
-}
+void Call_Report_Changes_Service() {
 
-//-----------------------------------------------------------
-// Prepare error response and send it.
-//-----------------------------------------------------------
-void send_err_resp_header(int status_code){
-	int len,size=12;
-	unsigned char ex_time;
-	char * myfifo = "/tmp/myfifo";
-	time_stamp_after = get_time_cs();
-	if((time_stamp_after-time_stamp_before) > 255){
-		ex_time = 255;
-	}else{
-		ex_time= time_stamp_after-time_stamp_before;
-	}
+	unsigned char command_code = MIRROR_REPORT_CHANGES;
 
-	response[RES_RI] = server_config_obj.raida_id;
-	response[RES_SH] = 0;
-	response[RES_SS] = status_code;
-	response[RES_EX] = 0;
-	response[RES_RE] = 0;
-	response[RES_RE+1] = 0;
-	response[RES_EC] = udp_buffer[REQ_EC];
-	response[RES_EC+1] = udp_buffer[REQ_EC+1];
-	response[RES_HS] = 0;
-	response[RES_HS+1] = 0;
-	response[RES_HS+2] = 0;
-	response[RES_HS+3] = 0;
-	len=sizeof(cliaddr);
-	
-	sendto(sockfd, (const char *)response, size,
-		MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-		len);
-	
-}
-//-----------------------------------------------------------
-// Prepare response and send it.
-//-----------------------------------------------------------
-void prepare_resp_header(unsigned char status_code){
-	unsigned char ex_time;
-	time_stamp_after = get_time_cs();
-	if((time_stamp_after-time_stamp_before) > 255){
-		ex_time = 255;
-	}else{
-		ex_time= time_stamp_after-time_stamp_before;
-	}
-
-	response[RES_RI] = server_config_obj.raida_id;
-	response[RES_SH] = 0;
-	response[RES_SS] = status_code;
-	response[RES_EX] = ex_time;
-	response[RES_RE] = 0;
-	response[RES_RE+1] = 0;
-	response[RES_EC] = udp_buffer[REQ_EC];
-	response[RES_EC+1] = udp_buffer[REQ_EC+1];
-	response[RES_HS] = 0;
-	response[RES_HS+1] = 0;
-	response[RES_HS+2] = 0;
-	response[RES_HS+3] = 0;
-
-}
-
-
-
-
-
-
-
-void Call_Mirror_Report_Changes() {
-
-//Assign timestamp bytes in the buffer
-// YY, MM, DD, HH, MM, SS
+	//Assign timestamp bytes in the buffer
+	// YY, MM, DD, HH, MM, SS
     send_req_buffer[index_req + 0] = tm.year;
     send_req_buffer[index_req + 1] = tm.month;
     send_req_buffer[index_req + 2] = tm.day;
@@ -305,11 +219,11 @@ void Call_Mirror_Report_Changes() {
     send_req_buffer[index_req + 4] = tm.minutes;
     send_req_buffer[index_req + 5] = tm.second;
 
-//Request body End bytes
+	//Request body End bytes
     send_req_buffer[index_req+TIMESTAMP_BYTES_CNT+0] = 62;
     send_req_buffer[index_req+TIMESTAMP_BYTES_CNT+1] = 62;
 
-    int len = index_req + TIMESTAMP_BYTES_CNT + CMD_END_BYTES_CNT;
+    unsigned int len = index_req + TIMESTAMP_BYTES_CNT + CMD_END_BYTES_CNT;
 
     printf("send_buffer:- ");
     for(int i=0; i < len; i++) {
@@ -317,6 +231,17 @@ void Call_Mirror_Report_Changes() {
     }
     printf("\n");
 
-    sendto(sockfd, (const char *)send_req_buffer, len, MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+	Send_Request(command_code, len);
+	Process_response_Report_Changes();
+
+}
+
+void Process_response_Report_Changes() {
+
+	Receive_response();
+	
+
+
+
 
 }
