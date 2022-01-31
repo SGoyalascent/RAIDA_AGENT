09402f8db26ef175983718afb8d2aa39981ad70c
SGoyalascent <shubham@ascentspark.com>
2022-01-31 13:36:54 +0530

    Update

--- Original_Primary_Agent/RAIDA_AGENT.c
+++ Original_Primary_Agent/RAIDA_AGENT.c
@@ -14,7 +14,7 @@ time_t t1 = 0;
 // Welcome Message
 //-----------------------------------------------
 void WelcomeMsg() {
-    printf("\nWelcome to the RAIDA Services\n");
+    printf("\nWelcome to the RAIDA AGENT\n");
 }
 //-------------------------------------------------
 //Get the Working Directory
@@ -148,20 +148,104 @@ void Read_Agent_Configuration_Files() {
     closedir(d);
 }
 
-//----------------------------------------------------------
-// Returns time in centi seconds
-//----------------------------------------------------------
-long get_time_cs()
+//-----------------------------------------------
+//READ KEYS.bin FILE and store in the RAM
+//-----------------------------------------------
+void read_keys_file() {
+    
+    FILE *fp = NULL;
+    int size = 0, ch;
+    char path[500];
+    char buff[KEY_BYTES_CNT*KEYS_COUNT];
+    strcpy(path, execpath);
+    strcat(path, "/Keys/keys.bin");
+    if((fp = fopen(path, "rb")) == NULL) {
+        printf("->Error: Keys.bin file cannot be opened\n");
+        return;
+    }
+    while((ch = fgetc(fp)) != EOF) {
+        size++;
+    }
+    printf("Keys_file_size: %d\n", size);
+    if(size != KEY_BYTES_CNT*KEYS_COUNT) {
+        printf("Error: Keys file size does not match. Keys missing\n");
+        return;
+    }
+    fclose(fp);
+
+    fp = fopen(path, "rb");
+    if(fread(buff, 1, size, fp) < size) {
+        printf("Keys bytes missing\n");
+        return;
+    }
+    fclose(fp);
+    
+    int index = 0;
+    for(int i = 0;i < KEYS_COUNT;i++) {
+        printf("KEY_%d: ", i+1);
+        memcpy(&keys_bytes[i][0], &buff[index], KEY_BYTES_CNT);
+        index += KEY_BYTES_CNT;
+        for(int j = 0; j < KEY_BYTES_CNT; j++) {
+            printf("%d ", keys_bytes[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+//-----------------------------------------------
+//GET LASTEST TIMESTAMP
+//-----------------------------------------------
+void get_latest_timestamp(char *path)
 {
-    long            ms,cs; // Milliseconds
-    time_t          s;  // Seconds
-    struct timespec spec;
-    clock_gettime(CLOCK_REALTIME, &spec);
-    s  = spec.tv_sec;
-    ms = round(spec.tv_nsec / 1.0e3); // Convert nanoseconds to milliseconds
-    cs = ms /100;	
-	printf("Current time: %"PRIdMAX".%03ld seconds since the Epoch\n",(intmax_t)s, ms);
-    return ms;	
+    struct dirent *dir; 
+    struct stat statbuf;
+    struct tm *dt;
+    char datestring[256];
+    time_t t2;
+    double time_dif;
+    DIR *d = opendir(path); 
+    if(d == NULL) {
+        return;  
+    }
+    while ((dir = readdir(d)) != NULL) 
+    {
+        char f_path[500], f_name[256];
+        sprintf(f_name, "%s",dir->d_name);
+        sprintf(f_path, "%s/%s", path, dir->d_name);
+
+        if((stat(f_path, &statbuf)) == -1) {
+            printf("Error: %s\n", strerror(errno));
+            continue;
+        }
+        //if regular file
+        if((statbuf.st_mode & S_IFMT) == S_IFREG) {
+            printf("filename: %s  filepath: %s\n", f_name, f_path);
+            dt = gmtime(&statbuf.st_mtime);
+            t2 = statbuf.st_mtime;    //modified time  mtime
+            strftime(datestring, sizeof(datestring), " %x-%X", dt);
+            printf("datestring: %s\n", datestring);
+
+            time_dif = difftime(t2, t1);
+            printf("time_diff: %g\n", time_dif);
+            if(time_dif > 0) {
+                t1 = t2;
+
+                tm.year = dt->tm_year;  //year from 1900 ==>  2021 == 121
+                tm.month = dt->tm_mon;  //month in 0 - 11 range  ==> 12(dec) == 11
+                tm.day = dt->tm_mday;
+                tm.hour = dt->tm_hour;
+                tm.minutes = dt->tm_min;
+                tm.second = dt->tm_sec;
+                printf("Last Modified Time(UTC):  %d-%d-%d  %d:%d:%d\n",tm.day, tm.month+1,tm.year+1900, tm.hour, tm.minutes, tm.second);
+            }
+        }
+        //if directory
+        else if(((statbuf.st_mode & S_IFMT) == S_IFDIR) && (strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0)) {
+            printf("dirname: %s  dirpath: %s\n", f_name, f_path);
+            get_latest_timestamp(f_path);
+        }
+    }
+    closedir(d);
 }
 
 int main() {
@@ -175,6 +259,25 @@ int main() {
     Read_Agent_Configuration_Files();
     read_keys_file();
 
+    char *path;
+    strcpy(path, execpath);
+    strcat(path, "/Data");
+    printf("-->MAIN: path: %s\n", path);
+    printf("-->MAIN: GET-LATEST-TIMESTAMP---\n");
+    get_latest_timestamp(path);
+
+    //echo_mirror_raida_server();
+    /*
+    int stat;
+    if((stat = strcmp(Agent_Mode, "primary")) == 0){
+
+    }
+    else if((stat = strcmp(Agent_Mode, "mirror"))) {
+
+    }
+    else if((stat = strcmp(Agent_Mode, "witness"))) {
+
+    }*/
 
     //Assume Primary RAIDA AGENT
 
